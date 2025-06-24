767b6ae49d47c30133af422d4b3088afa79c39cd
my-laptop <2655667253@qq.com>
2025-06-25 01:48:21 +0800

    添加自动化安装protobuf的插件辅助引入protobuf

---
+++ Plugins/SimpleProtobuf/GenerateProtobuf.bat
@@ -0,0 +1,3 @@
+start %~dp0Content\Engine\Binaries\Win64\ProtobufProgram.exe -CompressPath=%~dp0Content\google.rz -OutPutProtobufPath=E:\OneDrive\Unreal\UE5Project\GAS\RpgPolygon\RpgPolygon\Source\RpgPolygon\ThirdParty
+
+pause
---
+++ Plugins/SimpleProtobuf/GenerateProtobufCPP.bat
@@ -0,0 +1,3 @@
+%~dp0Content\Engine\Binaries\Win64\protoc.exe --cpp_out=E:\OneDrive\Unreal\UE5Project\GAS\RpgPolygon\RpgPolygon\Source\RpgPolygon\ThirdParty\GoogleProtobuf ./CPPScript/TestCPPProto.proto
+
+pause
